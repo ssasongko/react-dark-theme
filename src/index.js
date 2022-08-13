1d36c605c75b0d8e6950bc80a1da5a14f35cdc72
Nur Sasongko <kokonur1020@gmail.com>
2022-08-13 12:06:36 +0700

    Inspired by juniNguyen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
+import Background from './components/Background';
+import Toggle from './components/ThemeToggle';
+import { ThemeProvider } from './components/ThemeContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root')
+const root = createRoot(container)
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <ThemeProvider>
+        <Background>
+          <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+            <Toggle />
+          </div>
+        <App />
+        </Background>
+    </ThemeProvider>,
+  </React.StrictMode>,
 );
+
